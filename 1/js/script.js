c19cf7d2f5b699a960b63e1e127a949e1e5f8f32
BeishebaevEldar <148213957+BeishebaevEldar@users.noreply.github.com>
2024-02-03 21:41:34 +0600

    Add files via upload

---
+++ 1/js/script.js
@@ -0,0 +1,116 @@
+document.getElementById("main_img").onmouseenter = () => {
+  document.getElementById("hello").classList.add("active");
+};
+document.getElementById("main_img").onmouseout = () => {
+  document.getElementById("hello").classList.remove("active");
+};
+document.getElementById("hello").onmouseenter = () => {
+  document.getElementById("hello").classList.add("active");
+};
+document.getElementById("hello").onmouseout = () => {
+  document.getElementById("hello").classList.remove("active");
+};
+
+document.getElementById("main_img2").onmouseenter = () => {
+  document.getElementById("hi").classList.add("active");
+};
+document.getElementById("main_img2").onmouseout = () => {
+  document.getElementById("hi").classList.remove("active");
+};
+document.getElementById("hi").onmouseenter = () => {
+  document.getElementById("hi").classList.add("active");
+};
+document.getElementById("hi").onmouseout = () => {
+  document.getElementById("hi").classList.remove("active");
+};
+
+document.getElementById("main_img3").onmouseenter = () => {
+  document.getElementById("bye").classList.add("active");
+};
+document.getElementById("main_img3").onmouseout = () => {
+  document.getElementById("bye").classList.remove("active");
+};
+document.getElementById("bye").onmouseenter = () => {
+  document.getElementById("bye").classList.add("active");
+};
+document.getElementById("bye").onmouseout = () => {
+  document.getElementById("bye").classList.remove("active");
+};
+
+document.getElementById("main_img4").onmouseenter = () => {
+  document.getElementById("n1").classList.add("active");
+};
+document.getElementById("main_img4").onmouseout = () => {
+  document.getElementById("n1").classList.remove("active");
+};
+document.getElementById("n1").onmouseenter = () => {
+  document.getElementById("n1").classList.add("active");
+};
+document.getElementById("n1").onmouseout = () => {
+  document.getElementById("n1").classList.remove("active");
+};
+
+document.getElementById("main_img5").onmouseenter = () => {
+  document.getElementById("n2").classList.add("active");
+};
+document.getElementById("main_img5").onmouseout = () => {
+  document.getElementById("n2").classList.remove("active");
+};
+document.getElementById("n2").onmouseenter = () => {
+  document.getElementById("n2").classList.add("active");
+};
+document.getElementById("n2").onmouseout = () => {
+  document.getElementById("n2").classList.remove("active");
+};
+
+document.getElementById("main_img6").onmouseenter = () => {
+  document.getElementById("n2").classList.add("active");
+};
+document.getElementById("main_img6").onmouseout = () => {
+  document.getElementById("n6").classList.remove("active");
+};
+document.getElementById("n6").onmouseenter = () => {
+  document.getElementById("n6").classList.add("active");
+};
+document.getElementById("n6").onmouseout = () => {
+  document.getElementById("n6").classList.remove("active");
+};
+
+document.getElementById("main_img9").onmouseenter = () => {
+  document.getElementById("n9").classList.add("active");
+};
+document.getElementById("main_img9").onmouseout = () => {
+  document.getElementById("n9").classList.remove("active");
+};
+document.getElementById("n9").onmouseenter = () => {
+  document.getElementById("n9").classList.add("active");
+};
+document.getElementById("n9").onmouseout = () => {
+  document.getElementById("n9").classList.remove("active");
+};
+
+document.getElementById("main_img10").onmouseenter = () => {
+  document.getElementById("n10").classList.add("active");
+};
+document.getElementById("main_img10").onmouseout = () => {
+  document.getElementById("n10").classList.remove("active");
+};
+document.getElementById("n10").onmouseenter = () => {
+  document.getElementById("n10").classList.add("active");
+};
+document.getElementById("n10").onmouseout = () => {
+  document.getElementById("n10").classList.remove("active");
+};
+
+document.getElementById("main_img11").onmouseenter = () => {
+  document.getElementById("n11").classList.add("active");
+};
+document.getElementById("main_img11").onmouseout = () => {
+  document.getElementById("n11").classList.remove("active");
+};
+document.getElementById("n11").onmouseenter = () => {
+  document.getElementById("n11").classList.add("active");
+};
+document.getElementById("n11").onmouseout = () => {
+  document.getElementById("n11").classList.remove("active");
+};
